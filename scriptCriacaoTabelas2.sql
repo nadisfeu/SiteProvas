e85d235828a374622c5229407f07013f8322a9fa
Paulo Maturano Gonçalves Ramos <134672148+FoolPerformer@users.noreply.github.com>
2024-09-08 18:19:49 -0300

    Update scriptCriacaoTabelas2.sql

--- scriptCriacaoTabelas2.sql
+++ scriptCriacaoTabelas2.sql
@@ -2,13 +2,14 @@ DROP TABLE IF EXISTS atividade CASCADE;
 
 CREATE TABLE atividade
 (
-    "atvID" integer NOT NULL,
+    atvID integer NOT NULL,
     academico_email CHAR(22),
     institucao CHAR(22),
     disciplica CHAR(22),
-    "numQuest" integer,
-    "caminhoArquivo" CHAR(22),
-    PRIMARY KEY ("atvID")
+    numQuest integer,
+    caminhoArquivo CHAR(22),
+    PRIMARY KEY (atvID),
+	FOREIGN KEY (academico_email) REFERENCES academico (email)
 );
 
 DROP TABLE IF EXISTS academico CASCADE;
@@ -25,16 +26,21 @@ DROP TABLE IF EXISTS lista CASCADE;
 
 CREATE TABLE lista
 (
-    gabarito boolean DEFAULT false
+    gabarito boolean DEFAULT false,
+	PRIMARY KEY(atvID)
 )INHERITS (atividade);
 
-insert into lista values (1,'carlos@ufop', 'ufop','BD',)
+insert into lista values (1,'bruno@ufop', 'ufop','BD',10,'drive.com',true);
+insert into lista values (2,'ryan@ufop', 'ufop','AEDS 1',5,'drive.com',false);
+insert into lista values (2,'fernando@ufop', 'ufop','Programacao 1',6,'drive.com',false);
+insert into lista values (4,'paulo@ufop', 'ufop','Redes 1',9,'drive.com',true);
 
 DROP TABLE IF EXISTS prova CASCADE;
 
 CREATE TABLE prova
 (
-    tipo integer DEFAULT 1
+    tipo integer DEFAULT 1,
+	PRIMARY KEY(atvID)
 )INHERITS (atividade);
 
 DROP TABLE IF EXISTS professor CASCADE;
@@ -42,20 +48,27 @@ DROP TABLE IF EXISTS professor CASCADE;
 CREATE UNLOGGED TABLE professor
 (
     dep CHAR(22),
-    sala CHAR(22)
+    sala CHAR(22),
+	PRIMARY KEY(email)
 )INHERITS (academico);
 
 DROP TABLE IF EXISTS aluno CASCADE;
 
 CREATE TABLE aluno
 (
-    curso CHAR(22)
+    curso CHAR(22),
+	PRIMARY KEY(email)
 )INHERITS (academico);
 
 DROP TABLE IF EXISTS pesquisa CASCADE;
 
 CREATE TABLE pesquisa
 (
-    "atvID_atividade" integer,
-    email_academico CHAR(22)
+    atvID_atividade integer,
+    email_academico CHAR(22),
+
+	FOREIGN KEY (atvID_atividade) REFERENCES atividade (atvID),
+	FOREIGN KEY (email_academico) REFERENCES academico (email)
 );
+
+select * from lista;
