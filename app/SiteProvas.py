21abd3c9dc6eba72d23e11f13be53ccc8e20ad8b
RyanColen <ryancolen989898@gmail.com>
2024-09-22 15:25:49 -0300

    Update SiteProvas.py

--- app/SiteProvas.py
+++ app/SiteProvas.py
@@ -13,28 +13,51 @@ def verficar_email(email):
     regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
     return re.search(regex, email)
 
-
 #funcoes de insercao
-def _inserir_academico_(email, nome, instituicao, tipo):
-    insert_scrip = 'INSERT INTO ACADEMICO (email, nome, instituicao, tipo) VALUES (%s, %s, %s, %s)'
-    insert_values = (email, nome, instituicao,tipo)
+def inserir_academico(email, nome, tipo, instituicao):
+    insert_scrip = 'INSERT INTO ACADEMICO (email, nome, tipo, instituicao) VALUES (%s, %s, %s, %s);'
+    insert_values = (email, nome, tipo, instituicao)
     cur.execute(insert_scrip, insert_values)
 
 #CORRIGIR ERROS DE GRAMATICA EM ATIVIDADE NO BANCO DE DADOS/SCRIPT!!!!!!!
 def inserir_atividade(id, academico_email, instituicao, disciplina, num_request, caminho_arquivo):
-    insert_scrip = 'INSERT INTO ATIVIDADE (atvid, academico_email, instituicao, disciplina, numrequest, caminhoarquivo) VALUES (%s, %s, %s, %s, %s, %s)'
+    insert_scrip = 'INSERT INTO ATIVIDADE (atvid, academico_email, instituicao, disciplina, numQuest, caminhoArquivo) VALUES (%s, %s, %s, %s, %s, %s);'
     insert_values = (id, academico_email, instituicao, disciplina, num_request, caminho_arquivo)
     cur.execute(insert_scrip, insert_values)
 
-def inserir_lista(id, academico_email, instituicao, disciplina, num_request, caminho_arquivo, gabarito):
-    insert_scrip = 'INSERT INTO LISTA (atvid, academico_email, instituicao, disciplina, numrequest, caminhoarquivo, gabarito) VALUES (%s, %s, %s, %s, %s, %s, %s)'
-    insert_values = (id, academico_email, instituicao, disciplina, num_request, caminho_arquivo, gabarito)
+def inserir_lista(gabarito, id):
+    insert_scrip = 'INSERT INTO LISTA (gabarito, atvID) VALUES (%s, %s);'
+    insert_values = (gabarito, id)
     cur.execute(insert_scrip, insert_values)
 
-def inserir_prova(id, academico_email, instituicao, disciplina, num_request, caminho_arquivo, tipo):
-    insert_scrip = 'INSERT INTO LISTA (atvid, academico_email, instituicao, disciplina, numrequest, caminhoarquivo, tipo) VALUES (%s, %s, %s, %s, %s, %s, %s)'
-    insert_values = (id, academico_email, instituicao, disciplina, num_request, caminho_arquivo, tipo)
+def inserir_conteudo(materia, id):
+    insert_scrip = 'INSERT INTO LISTA (gabarito, atvID) VALUES (%s, %s);'
+    insert_values = (materia, id)
     cur.execute(insert_scrip, insert_values)
+
+def inserir_prova(tipo, id):
+    insert_scrip = 'INSERT INTO LISTA (tipo, atvID) VALUES (%s, %s);'
+    insert_values = (tipo, id)
+    cur.execute(insert_scrip, insert_values)
+
+def inserir_pesquisa(id_atividade, email_academico):
+    insert_script = 'INSERT INTO PESQUISA (atvID_atividade, email_academico) VALUES (%s, %s);'
+    insert_values = (id_atividade, email_academico)
+    cur.execute(insert_script, insert_values)
+
+def pesquisa(disciplina, tipo):
+    if tipo == 'prova':
+        select_script = 'select disciplina, tipo, caminhoArquivo from atividade LEFT JOIN prova where disciplina='+disciplina+';'
+        cur.execute(select_script)
+        dados = cur.fetchall()
+        return dados
+    elif tipo == 'lista':
+        select_script = 'select disciplina, gabarito, caminhoArquivo from atividade LEFT JOIN lista where disciplina='+disciplina+';'
+        cur.execute(select_script)
+        dados = cur.fetchall()
+        return dados
+    else:
+        print("\nNao foi possivel realizar a pesquisa")
 
 
 #conexao com o servidor do bd
@@ -47,14 +70,38 @@ try:
     email = input("\nDigite seu email: ")
     nome = input("\nDigite seu nome: ")
     instituicao = input("\nQual e sua instituicao?")
-    curso = input("\nInforme seu curso: ")
-    #inserir_aluno('fernando@gmail.com', 'Fernando', 'ufop', 'computacao') #apenas teste
-    if verficar_email(email):
-        inserir_aluno(email, nome, instituicao, curso)
-        print("\nConta registrada com sucesso!")
-    else:
+    tipo = input("Voce e docente?")
+    if not verficar_email(email):
         print("\nEmail invalido!")
-    conn.commit()
+
+    try:
+        inserir_academico(email, nome, tipo, instituicao)
+        conn.commit()
+    except Exception as error:
+        print(error)
+
+    #conn.commit()
+    while True:
+        opcao = input("\nDeseja enviar ou pesquisar uma atividade? Digite sair para encerrar: ").lower()
+        print(opcao)
+        if opcao == 'sair':
+            break
+        elif opcao != 'enviar' or opcao != 'pesquisar':
+            print("\nOpcao invalida!")
+        else:
+            if opcao == 'pesquisar':
+                tipo = input("\nPesquisar por uma lista ou prova")
+                disciplina = input("\nQual disicplina?")
+                dados = pesquisa(disciplina, tipo)
+                for linha in dados:
+                    print(linha)
+                #inserir a pesquisa na tabela pesquisa do bd
+            elif opcao == 'enviar':
+                #input para receber e registrar atividade no bd
+                print("a")
+            else:
+                print("\nOpcao invalida!")
+
 
 
 
